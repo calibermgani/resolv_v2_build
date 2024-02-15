55f1473ee22d46702d228c423447a5f7f3ad0fac
SelvaFront <selva@caliberfocus.com>
2024-02-15 18:15:26 +0530

    Changes

--- backend/app/Http/Controllers/Userregister.php
+++ backend/app/Http/Controllers/Userregister.php
@@ -134,7 +134,6 @@ $token = JWTAuth::encode($payload);*/
 
         //Core DB data Creation
         $user_core = User::on('mysql')->create($user_tab_data);
-        dd($user_core);
         $address = Address_flag::create([
             // 'address_company'      => 'usps',
             // 'type'                 => 'users',
@@ -243,7 +242,7 @@ $token = JWTAuth::encode($payload);*/
             }
         }
         // \Config::get('database')
-
+        dd(1);
         return response()->json([
             'data' => $dbs,
             'error' => 'Registration Successful'
