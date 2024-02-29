a44756f96a2484ca2452090fb5dfbe846498eca0
ashique-caliber <mashique@caliberfocus.com>
2024-02-29 20:21:00 +0530

    import changes

--- backend/app/Imports/ImportNewClaims.php
+++ backend/app/Imports/ImportNewClaims.php
@@ -117,7 +117,7 @@ class ImportNewClaims implements ToCollection, WithHeadingRow
                                 $val[$field] = $val[$field];
                             }
                         }
-                        dump($val[$field]);
+                        dump($index_ip, $val[$field]);
                     }
                 }
 
